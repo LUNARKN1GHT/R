f3bee654b34cb9591877a62a9f1cdefe1d6f4ffa
IVANLEE <lee162286@gmail.com>
2026-01-20 17:16:18 +0800

    表达式

---
+++ programs/chapter_5/expression.R
@@ -0,0 +1,15 @@
+# 表达式
+
+# R 代码是由一系列表达式组成的。
+# R 中的表达式的例子包括赋值语句、条件语句和算数表达式。
+# 下面是几个表达式的例子
+x <- 1
+if (1 > 2) "yes" else "no"
+127 %% 10
+
+# 表达式由对象和函数组成，可以用华纳会给你付或者分号来分割表达式。
+## 这里依然被formater发力进行了换行。
+# "this expression will be printed"; 7 + 13; exp(0+1i*pi)
+"this expression will be printed"
+7 + 13
+exp(0 + 1i * pi)
