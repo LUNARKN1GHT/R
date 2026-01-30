12193be2a13f801d5795212b005d173d27abb451
IVANLEE <lee162286@gmail.com>
2026-01-30 10:24:59 +0800

    下载数据集

---
+++ projects/climate_change/Climate_Data_Download.R
@@ -0,0 +1,6 @@
+# 自动下载全球气温汇总数据 (精简版)
+url <- "https://raw.githubusercontent.com/datasets/global-temp/master/data/annual.csv"
+climate_raw <- read.csv(url)
+
+# 保存到本地 data 文件夹
+write.csv(climate_raw, "data/global_temp_annual.csv", row.names = FALSE)
