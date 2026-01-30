1083e98ac0a47fcfd9d534dac6feff037aa32e40
IVANLEE <lee162286@gmail.com>
2026-01-30 10:27:48 +0800

    修改数据存储路径

--- projects/climate_change/Climate_Data_Download.R
+++ projects/climate_change/Climate_Data_Download.R
@@ -3,4 +3,4 @@ url <- "https://raw.githubusercontent.com/datasets/global-temp/master/data/annua
 climate_raw <- read.csv(url)
 
 # 保存到本地 data 文件夹
-write.csv(climate_raw, "data/global_temp_annual.csv", row.names = FALSE)
+write.csv(climate_raw, "data/climate_change/global_temp_annual.csv", row.names = FALSE)
